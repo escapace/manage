97872fd8b46706a60546c32d128b291898dc2643
l5x <189198+l5x@users.noreply.github.com>
2017-06-01 01:18:52 +0000

    remove: bats & bats tests

--- test/test-helper-external.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source "${BATS_TEST_DIRNAME}/test-helper-generic.bash"
-
-export BATSMANAGELIBEXEC="${BATS_PARENT_DIRNAME}/libexec"
-export BATSMANAGEDIRECTORY="${BATS_PARENT_DIRNAME}"
-
-unset MANAGEREPOSITORY
-unset MANAGEDIRECTORY
