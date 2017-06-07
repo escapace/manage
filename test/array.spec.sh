483c9281536ba0136e143d0c4416daf9d92b18ba
Mark Milstein <mark@epiloque.com>
2017-06-07 05:27:18 +0000

    change: proper global manage variable naming
    Signed-off-by: Mark Milstein <mark@epiloque.com>

--- test/array.spec.sh
+++ test/array.spec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2016,SC1090
 
-source "${MANAGEDIRECTORY}/bin/manage"
+source "${MANAGE_DIRECTORY}/bin/manage"
 
 MANAGE_UNDERSCORE \
     array/complement \
